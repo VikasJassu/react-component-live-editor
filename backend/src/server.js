1d27fb6405ed1392d7b84c2408e3b9fc3f5a4920
VikasJassu <vksjassu@gmail.com>
2025-08-14 20:36:43 +0530

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,7 +40,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "production"
-        ? ["https://your-frontend-domain.com"]
+        ? ["https://react-component-live-editor.vercel.app"]
         : ["http://localhost:5174", "http://localhost:3000"],
     credentials: true,
   })
